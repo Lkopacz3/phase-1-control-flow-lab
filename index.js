1a9764a76d46860bfcbeec89f69d2248b98d19e3
Lee Kopacz <lee_kopacz@yahoo.com>
2022-10-12 23:44:49 -0500

    commit tree 5dc44d6ef2bebe36692b2e0eecb51872f5e1625c

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(value) {
+
+  let greeting;
+
+  if (value <= 400 ){
+  greeting = ("This one is on me!")
+  } else if (value >= 401 && value <= 2000 ){
+  greeting = ("That will be twenty bucks.")
+  } else if (value >= 2000 && value <=2499){
+  greeting = ("I will gladly take your thirty bucks.")
+  } else if (value >= 2500){
+    greeting = ("No can do.")
+  }
+  return greeting
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity() {
+ const city = "NYC"
+const cityCheck = city >= "Pittsburgh" ? "Ok, sounds good." : "No go."
+return cityCheck
 }
+
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+ 
+  const tip = "generous";
+  let charm;
+
+ switch(tip) {
+  case "generous":
+    charm = "Thank you so much.";
+    break;
+  case "not as generous":
+    charm = "Thank you.";
+  default:
+    charm = "Bye."
+  
+ }
+return charm;
 }
